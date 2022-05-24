e2880853ff5f8abe434cfe65bf4eb1831c191553
prooni <lhw629@naver.com>
2022-05-24 11:08:28 +0900

    [Silver III] Title: N과 M (1), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void dfs(int k);
+
+int main()
+{
+    cin >> N >> M;
+
+    dfs(0);
+
+    return 0;
+}
+
+void dfs(int k)
+{
+    if (k == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+    }
+    else
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            if (visited[i] == false)
+            {
+                arr[k] = i;
+                visited[i] = true;
+                dfs(k+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
