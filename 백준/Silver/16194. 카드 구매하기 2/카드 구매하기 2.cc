e12de69a7f03af02eb068905354409e88231cc3b
prooni <lhw629@naver.com>
2024-01-27 11:26:28 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int D[1000001];
+
+int DP(vector<int> vec, int n);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> vec;
+    vec.push_back(-1);
+    int M, N;
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> M;
+        vec.push_back(M);
+    }
+    cout << DP(vec, N);
+    return 0;
+}
+
+int DP(vector<int> vec, int n)
+{
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        D[i] = 1000 * 10000;
+    }
+    D[0] = 0;
+    for (int i = 1; i < vec.size(); ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            D[i] = min(vec[j] + D[i - j], D[i]);
+        }
+    }
+    return D[n];
+}
