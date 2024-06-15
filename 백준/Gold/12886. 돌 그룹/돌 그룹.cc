02e97eb9fb76b7a0f7891be91cc17551f49b5d43
prooni <lhw629@naver.com>
2024-06-15 17:51:55 +0900

    [Gold IV] Title: 돌 그룹, Time: 24 ms, Memory: 7456 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.cc
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.cc
@@ -1,51 +1,79 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <queue>
+
 using namespace std;
-int ck[2001][2001];
-int bfs(int a,int b, int c){
-    queue <tuple<int,int,int>> q;
-    q.push({a,b,c});
-    ck[a][b] = 1;
-    ck[b][a] = 1;
-    ck[a][c] = 1;
-    ck[c][a] = 1;
-    ck[b][c] = 1;
-    ck[c][b] = 1;
-    while(!q.empty()){
-        int x = get<0>(q.front());
-        int y = get<1>(q.front());
-        int z = get<2>(q.front());
+
+bool visited[2001][2001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    queue<tuple<int, int, int>> q;
+    q.push(make_tuple(A, B, C));
+
+    visited[A][B] = 1;
+    visited[B][A] = 1;
+    visited[A][C] = 1;
+    visited[C][A] = 1;
+    visited[B][C] = 1;
+    visited[C][B] = 1;
+
+    while (!q.empty())
+    {
+        tuple<int, int, int> front = q.front();
+        int a = get<0>(front);
+        int b = get<1>(front);
+        int c = get<2>(front);
+
         q.pop();
-        if(x == y && y == z) return 1;
-        if(y - x > 0 && !ck[x+x][y-x]){
-            q.push({x+x,y-x,z});
-            ck[x+x][y-x] = 1;
+
+        if (a == b && b == c)
+        {
+            cout << 1;
+            return 0;
         }
-        if(x - y > 0 && !ck[x-y][y+y]){
-            q.push({x-y,y+y,z});
-            ck[x-y][y+y] = 1;
+
+        if (a - b > 0 && !visited[a - b][b + b])
+        {
+            visited[a - b][b + b] = true;
+            q.push(make_tuple(a - b, b + b, c));
         }
-        if(z - x > 0 && !ck[x+x][z-x]){
-            q.push({x+x,y,z-x});
-            ck[x+x][z-x] = 1;
+        if (b - a > 0 && !visited[a + a][b - a])
+        {
+            visited[a + a][b - a] = true;
+            q.push(make_tuple(a + a, b - a, c));
         }
-        if(x - z > 0 && !ck[x-z][z+z]){
-            ck[x-z][z+z] = 1;
-            q.push({x-z,y,z+z});
+        if (b - c > 0 && !visited[b - c][c + c])
+        {
+            visited[b - c][c + c] = true;
+            q.push(make_tuple(a, b - c, c + c));
         }
-        if(z - y > 0 && !ck[y+y][z-y]){
-            ck[y+y][z-y] = 1;
-            q.push({x,y+y,z-y});
+        if (c - b > 0 && !visited[b + b][c - b])
+        {
+            visited[b + b][c - b] = true;
+            q.push(make_tuple(a, b + b, c - b));
         }
-        if(y - z > 0 && !ck[y-z][z+z]){
-            ck[y-z][z+z] = 1;
-            q.push({x,y-z,z+z});
+        if (a - c > 0 && !visited[a - c][c + c])
+        {
+            visited[a - c][c + c] = true;
+            q.push(make_tuple(a - c, b, c + c));
+        }
+        if (c - a > 0 && !visited[a + a][c - a])
+        {
+            visited[a + a][c - a] = true;
+            q.push(make_tuple(a + a, b, c - a));
         }
     }
+    
+    cout << 0;
     return 0;
-}
-
-int main(){
-    int a,b,c;
-    cin >> a >> b >> c;
-    cout << bfs(a,b,c);
 }
