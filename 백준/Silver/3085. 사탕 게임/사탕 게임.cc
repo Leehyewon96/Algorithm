f13e22f436970fb8d86f08b6c4a1fbd3e89d6cca
prooni <lhw629@naver.com>
2024-08-05 11:49:51 +0900

    [Silver II] Title: 사탕 게임, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.cc
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.cc
@@ -1,95 +1,193 @@
 #include <iostream>
-#include <string>
-#include <stack>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
 
-int Check(vector<string> &arr);
+char arr[51][51];
+int ans = 0;
+
+void Check(int x, int y, int n)
+{
+	if (x + 1 < n)
+	{
+		int temp = arr[x][y];
+		arr[x][y] = arr[x + 1][y];
+		arr[x + 1][y] = temp;
+
+		int cnt = 1;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (arr[x][i] == arr[x][i + 1])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
+
+		cnt = 1;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (arr[x + 1][i] == arr[x + 1][i + 1])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
+
+		cnt = 1;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (arr[i][y] == arr[i + 1][y])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
+
+
+		temp = arr[x][y];
+		arr[x][y] = arr[x + 1][y];
+		arr[x + 1][y] = temp;
+	}
+
+	if (y + 1 < n)
+	{
+		int temp = arr[x][y];
+		arr[x][y] = arr[x][y + 1];
+		arr[x][y + 1] = temp;
+
+		int cnt = 1;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (arr[i][y] == arr[i + 1][y])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
+
+		cnt = 1;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (arr[i][y + 1] == arr[i + 1][y + 1])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
+
+		cnt = 1;
+		for (int i = 0; i < n - 1; ++i)
+		{
+			if (arr[x][i] == arr[x][i + 1])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
+
+
+		temp = arr[x][y];
+		arr[x][y] = arr[x][y + 1];
+		arr[x][y + 1] = temp;
+	}
+}
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int N;
-    cin >> N;
+	int N;
+	cin >> N;
 
-    vector<string> a(N);
+	string str;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> str;
+		for (int j = 0; j < N; ++j)
+		{
+			arr[j][i] = str[j];
+		}
+	}
 
-    for (int i = 0; i < N; ++i)
-    {
-        cin >> a[i];
-    }
+	
+	for (int i = 0; i < N; ++i)
+	{
+		int cnt = 1;
+		for (int j = 0; j < N - 1; ++j)
+		{
+			if (arr[j][i] == arr[j + 1][i])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
 
-    int maxResult = 1;
-    for (int i = 0; i < a.size(); ++i)
-    {
-        for (int j = 0; j < a.size() - 1; ++j)
-        {
-            char temp = a[i][j];
-            a[i][j] = a[i][j + 1];
-            a[i][j + 1] = temp;
+		cnt = 1;
+		for (int j = 0; j < N - 1; ++j)
+		{
+			if (arr[i][j] == arr[i][j + 1])
+			{
+				cnt++;
+			}
+			else
+			{
+				ans = max(ans, cnt);
+				cnt = 1;
+			}
+		}
+		ans = max(ans, cnt);
 
-            maxResult = max(maxResult, Check(a));
+	}
 
-            temp = a[i][j];
-            a[i][j] = a[i][j + 1];
-            a[i][j + 1] = temp;
-        }
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			Check(j, i, N);
+		}
+	}
+	
 
-        for (int j = 0; j < a.size() - 1; ++j)
-        {
-            char temp = a[j][i];
-            a[j][i] = a[j+1][i];
-            a[j+1][i] = temp;
+	cout << ans;
 
-            maxResult = max(maxResult, Check(a));
 
-            temp = a[j][i];
-            a[j][i] = a[j + 1][i];
-            a[j + 1][i] = temp;
-        }
-    }
-
-    cout << maxResult << '\n';
-    return 0;
-}
-
-int Check(vector<string> &arr)
-{
-    int maxCnt = 1;
-    for (int i = 0; i < arr.size(); ++i)
-    {
-        int cnt = 1;
-        for (int j = 0; j < arr.size() - 1; ++j)
-        {
-            if (arr[i][j] == arr[i][j + 1])
-            {
-                ++cnt;
-            }
-            else
-            {
-                cnt = 1;
-            }
-            maxCnt = max(maxCnt, cnt);
-        }
-
-        cnt = 1;
-        for (int j = 0; j < arr.size() - 1; ++j)
-        {
-            if (arr[j][i] == arr[j + 1][i])
-            {
-                ++cnt;
-            }
-            else
-            {
-                cnt = 1;
-            }
-            maxCnt = max(maxCnt, cnt);
-        }
-    }
-
-    return maxCnt;
+	return 0;
 }
