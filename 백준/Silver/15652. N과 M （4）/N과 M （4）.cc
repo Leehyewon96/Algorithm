41e766bda2330624824335177199450f65ac6d14
prooni <lhw629@naver.com>
2024-08-06 10:19:33 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//bool visited[9];
+
+void DFS(int m, int n, vector<int>& pick)
+{
+	if (pick.size() == m)
+	{
+		for (int i = 0; i < pick.size(); ++i)
+		{
+			cout << pick[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= n; ++i)
+	{
+		/*if (visited[i])
+		{
+			continue;
+		}*/
+		//visited[i] = true;
+		if (pick.size() > 0 && pick.back() > i)
+		{
+			continue;
+		}
+		pick.push_back(i);
+		DFS(m, n, pick);
+		pick.pop_back();
+		//visited[i] = false;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+	vector<int> pick;
+	DFS(M, N, pick);
+
+	return 0;
+}
