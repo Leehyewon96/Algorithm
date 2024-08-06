fee1b68bbb528100e7954a8c8fbcc4df247a3e6a
prooni <lhw629@naver.com>
2024-08-06 11:56:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,45 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <memory.h>
 using namespace std;
-#define MAX 60
+int ans = 0;
 
-int N;
-int methodNum = 0;
-
-
-void DFS(int sum, int inputNum)
+void DFS(int sum, const int num)
 {
-    if (sum > inputNum)
-    {
-        return;
-    }
+	if (sum == num)
+	{
+		ans++;
+		return;
+	}
 
-    if (sum == inputNum)
-    {
-        ++methodNum;
-    }
+	if (sum > num)
+	{
+		return;
+	}
 
-    for (int i = 1; i <= 3; ++i)
-    {
-        DFS(sum + i, inputNum);
-    }
+	for (int i = 1; i <= 3; ++i)
+	{
+		sum += i;
+		DFS(sum, num);
+		sum -= i;
+	}
 }
-
 
 int main()
 {
-    cin >> N;
-    vector<int> inputVec;
-    int inputNum = 0;
-    for (int i = 0; i < N; ++i)
-    {
-        cin >> inputNum;
+	int T;
+	cin >> T;
 
-        DFS(0, inputNum);
-        cout << methodNum << '\n';
-        methodNum = 0;
-    }
-    return 0;
+	int num;
+	while (T--)
+	{
+		cin >> num;
+		DFS(0, num);
+		cout << ans << '\n';
+		ans = 0;
+	}
+	return 0;
 }
