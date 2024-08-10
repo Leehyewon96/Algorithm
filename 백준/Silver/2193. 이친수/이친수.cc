3786d6ed014a1d5b63cde214d5838f255f06f26d
prooni <lhw629@naver.com>
2024-08-10 20:58:15 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -1,34 +1,33 @@
 #include <iostream>
-#include <string>
-#include <stack>
 #include <vector>
+#include <string>
 #include <algorithm>
-#include <cmath>
-using namespace std;
-#define MOD 1000000000;
+#include <memory.h>
 
-long long D[100001];
-long long DP(int N);
+using namespace std;
+
+long long dp[91][2];
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	cin.tie(0);
+	cin.sync_with_stdio(0);
 
-    int N;
-    cin >> N;
-    cout << DP(N);
-    return 0;
-}
+	int N;
+	cin >> N;
 
-long long DP(int N)
-{
-    D[1] = 1;
-    D[2] = 1;
-    if (D[N] > 0)
-    {
-        return D[N];
-    }
-    D[N] = DP(N - 1) + DP(N - 2);
-    return D[N];
+	dp[1][1] = 1;
+
+	for (int i = 2; i <= N; ++i)
+	{
+		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+		dp[i][1] = dp[i - 1][0];
+	}
+	
+	//dp[n][0] = dp[n - 1][0] + dp[n - 1][1];
+	//dp[n][1] = dp[n - 1][0];
+
+	cout << dp[N][0] + dp[N][1] << '\n';
+	
+	return 0;
 }
