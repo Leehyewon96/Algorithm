dd142371b52423ad73d68c476c8a07a75f47b0fe
prooni <lhw629@naver.com>
2024-08-10 20:13:38 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -1,46 +1,36 @@
 #include <iostream>
-#include <string>
-#include <stack>
 #include <vector>
+#include <string>
 #include <algorithm>
-#include <cmath>
+#include <memory.h>
 using namespace std;
 
-int D[1000001];
+int dp[1001];
 
-int DP(vector<int> vec, int n);
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	cin.tie(0);
+	cin.sync_with_stdio(0);
+	
+	int N; 
+	cin >> N;
+	vector<int> costVec(N + 1);
+	for (int i = 1; i <= N; ++i)
+	{
+		cin >> costVec[i];
+		dp[i] = costVec[i];
+	}
 
-    vector<int> vec;
-    vec.push_back(-1);
-    int M, N;
-    cin >> N;
-    for (int i = 0; i < N; ++i)
-    {
-        cin >> M;
-        vec.push_back(M);
-    }
-    cout << DP(vec, N);
-    return 0;
-}
+	for (int i = 1; i <= N; ++i)
+	{
+		for (int j = 1; j < i; ++j)
+		{
+			dp[i] = min(dp[i], dp[i - j] + dp[j]);
+		}
+	}
 
-int DP(vector<int> vec, int n)
-{
-    for (int i = 0; i < vec.size(); ++i)
-    {
-        D[i] = 1000 * 10000;
-    }
-    D[0] = 0;
-    for (int i = 1; i < vec.size(); ++i)
-    {
-        for (int j = 1; j <= i; ++j)
-        {
-            D[i] = min(vec[j] + D[i - j], D[i]);
-        }
-    }
-    return D[n];
+	cout << dp[N];
+
+	return 0;
 }
